afaaff22cfca80e5f5c5cb4c29d435e563217b35
abhijeetsahu09 <abhijeetsahu937@gmail.com>
2023-09-14 23:25:24 +0530

    commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/prog.jpg";
+import image from "../images/prog1.jpg";
 
 const imageAltText = "Programmer World...... Hello World!!";
 
